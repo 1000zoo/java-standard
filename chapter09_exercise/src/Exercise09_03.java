2295efbdf88b071e2c0c5bd5391331e14b1bf69f
1000zoo <cjswldn99@gmail.com>
2022-06-09 18:38:46 +0900

    chap9 exercise fin

---
+++ chapter09_exercise/src/Exercise09_03.java
@@ -0,0 +1,15 @@
+public class Exercise09_03 {
+        public static void main(String[] args) {
+            String fullPath = "c:\\jdk1.5\\work\\PathSeparateTest.java";
+            String path = "";
+            String fileName = "";
+            int pos = fullPath.lastIndexOf("\\");
+            if(pos!=-1) {
+                path = fullPath.substring(0, pos);
+                fileName = fullPath.substring(pos+1);
+            }
+            System.out.println("fullPath:"+fullPath);
+            System.out.println("path:"+path);
+            System.out.println("fileName:"+fileName);
+        }
+}
---
+++ chapter09_exercise/src/Exercise09_04.java
@@ -0,0 +1,13 @@
+public class Exercise09_04 {
+    static void printGraph(int[] dataArr, char ch) {
+        for(int i=0; i < dataArr.length;i++) {
+            for(int j=0; j < dataArr[i];j++) {
+                System.out.print(ch);
+            }
+            System.out.println(dataArr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        printGraph(new int[]{3,7,1,4},'*');
+    }
+}
